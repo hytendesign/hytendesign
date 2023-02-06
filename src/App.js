030f59b72137414e3b1eace0b32d1fe9445361bc
DanielMcManus <daniel.mcmanus@ffs.co.uk>
2023-02-06 19:59:25 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hyten Design</h1>
+        <p>Web Design and Development<br/>Comming Soon</p>
+        <button className="App-btn" href="mailto:daniel@hytendesign.com">Get in Touch</button>
+      </header>
+      <footer>
+        <div className="App-footer">
+          <p>Hyten Design</p>
+          <p>Email: <a className="App-link" href="mailto:daniel@hytendesign.com">daniel@hytendesign.com</a></p>
+          <p>Tel: 07854970411</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
