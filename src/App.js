ba92f6c73135357f46c5426b82a562df7e6617ed
DanielMcManus <daniel.mcmanus@ffs.co.uk>
2023-02-09 21:34:03 +0000

    Updates to logos and colours

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
+import logo from './hyten-design-logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hyten Design</h1>
-        <p>Web Design and Development<br/>Comming Soon</p>
-        <button className="App-btn" href="mailto:daniel@hytendesign.com">Get in Touch</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Web Design and Development</p>
+        <button className="App-link" href="mailto:daniel@hytendesign.com">Get in Touch</button>
       </header>
       <footer>
         <div className="App-footer">
-          <p>Hyten Design</p>
-          <p>Email: <a className="App-link" href="mailto:daniel@hytendesign.com">daniel@hytendesign.com</a></p>
-          <p>Tel: 07854970411</p>
+          <p>&#169;2023 Hyten Design</p>
+          <p >Email: <a className="App-email" href="mailto:daniel@hytendesign.com">daniel@hytendesign.com</a></p>
+          <p>Tel: +44 07854 970411</p>
         </div>
       </footer>
     </div>
